12b4e510e6825c8d49b308092907e0425d1d7c9f
Nithish Reddy <nithishreddyb2@gmail.com>
2025-09-12 13:28:21 +0530

    added the mongo URI

--- app/config.py
+++ app/config.py
@@ -1,10 +1,1 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://admin:test1234@pieceofmind.rbtpkei.mongodb.net/?retryWrites=true&w=majority&appName=PieceOfMind"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-db = client.todo_db
-collection = db("todo_data")
+MONGO_URI = "mongodb+srv://admin:test1234@pieceofmind.rbtpkei.mongodb.net/?retryWrites=true&w=majority&appName=PieceOfMind"
